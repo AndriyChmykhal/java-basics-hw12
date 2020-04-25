f92661eae1c10d78dcd18dc60369056ca4b2310a
AndriyChmykhal <chmykhal@ukr.net>
2020-04-26 00:41:38 +0300

    getSumSqueareEven and getSortedOddsArray

--- src/StreamTask.java
+++ src/StreamTask.java
@@ -1,12 +1,30 @@
+import java.util.Arrays;
+
 public class StreamTask {
 
     public static int getSumSquareEven(int[] numbers) {
-        // TODO replace return with your code
-        return 0;
+        return Arrays.stream(numbers)
+                .filter(i -> i % 2 == 0)
+                .map(i -> i * i)
+                .sum();
     }
 
     public static int[] getSortedOddsArray(int[] numbers) {
-        // TODO replace return with your code
-        return new int[]{};
+        return Arrays.stream(numbers)
+                .filter(i -> i % 2 != 0)
+                .sorted()
+                .toArray();
     }
 }
+
+
+/*getSumSquareEven
+It should accept int array, filter only even numbers,
+and return sum of its square values.
+Please use Stream API for this task. Avoid any loops.
+
+getSortedOddsArray
+It should accept int array, filter only odd numbers,
+and return as int array sorted in ascending order.
+Please use Stream API for this task.
+Avoid any loops. All test should pass.*/
